51b88447d1a4e041567855bd989b8f4f0149942e
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-03-22 13:48:03 +0100

    Update the docker circleci image

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -1,10 +1,18 @@
 import puppeteer from 'puppeteer';
 
+/**
+ * Scrape the content of any url getting the rendered html
+ * @param  { string } url - url to load
+ * @param  { string } selector - DOM selector to filter the resulting html
+ * @return { string } html result
+ */
 export async function scrape(url, selector = 'body') {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
     await page.goto(url);
     const html = await page.$eval(selector, e => e.innerHTML);
+
     await browser.close();
 
     return html;
