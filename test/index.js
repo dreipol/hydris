ee4e616d24661e028b2db8c76dbafad2e04e59de
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-03-29 16:12:59 +0200

    Re enable the server tests for circle ci

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@ const bin = require('../bin');
 const hookStdout = require('./hook-stdout');
 const hydris = require('../');
 const { get } = require('http');
-const isCircle = process.env.CIRCLECI;
 
 /*eslint max-nested-callbacks: ["error", 6]*/
 
@@ -16,6 +15,8 @@ function teardownServer(server) {
 }
 
 describe('hydris', function() {
+    this.timeout = 100000;
+
     it('hydris is alive', () => {
         assert.ok(hydris);
     });
@@ -31,25 +32,22 @@ describe('hydris', function() {
         });
     });
 
-    // skip this test for the Circle ci for now
-    if (!isCircle) {
-        describe('hydris server', () => {
-            it('Can start the server', (done) => {
-                const port = 8080;
-                const server = hydris.server.start({ port });
+    describe('hydris server', () => {
+        it('Can start the server', (done) => {
+            const port = 8080;
+            const server = hydris.server.start({ port });
 
-                get({
-                    hostname: '127.0.0.1',
-                    port,
-                    path: `?url=${ localpath('fixtures/index.html') }&node=#root`,
-                }, (response) => {
-                    assert(response.statusCode, 200);
-                    teardownServer(server);
-                    done();
-                });
+            get({
+                hostname: '127.0.0.1',
+                port,
+                path: `?url=${ localpath('fixtures/index.html') }&node=#root`,
+            }, (response) => {
+                assert(response.statusCode, 200);
+                teardownServer(server);
+                done();
             });
         });
-    }
+    });
 
     describe('hydris bin', () => {
         it('It can scrape an url', (done) => {
